83896ecec4a2e70450635aa52e1b7819478c169a
martkir <martinbarut@hotmail.com>
2019-03-03 20:04:38 +0000

    added sh script

--- scripts/cifar10_standard_single_gpu_tutorial.sh
+++ scripts/cifar10_standard_single_gpu_tutorial.sh
@@ -58,3 +58,4 @@ cd ..
 #python data_experiments.py --label "truck" --seed 28 --num_epochs 120 --target_percentage 10 --train_data 'reduced'
 #python data_experiments.py --label "truck" --seed 27 --num_epochs 120 --target_percentage 10 --train_data 'reduced'
 python data_experiments.py --label "truck" --seed 26 --num_epochs 120 --target_percentage 10 --train_data 'reduced'
+
