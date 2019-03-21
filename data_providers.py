8690c441834c3cf7b74e477f265b2c5ff0cd09f7
Ashe Magalhaes <ashemagalhaes@c103014.wlan.net.ed.ac.uk>
2019-03-21 16:18:36 +0000

    testing

--- data_providers.py
+++ data_providers.py
@@ -42,7 +42,6 @@ import torch
 import torch.multiprocessing as multiprocessing
 from torch._C import _set_worker_signal_handlers, _update_worker_pids, \
     _remove_worker_pids, _error_if_any_worker_fails
-from . import SequentialSampler, RandomSampler, BatchSampler
 import signal
 import functools
 from torch._six import container_abcs
